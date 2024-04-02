62892191ad68fda736c015165aeebdfe2a422e83
Thosoky <ryabonova@mail.ru>
2024-04-02 23:50:01 +0700

    add Mult n Div

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@
         Console.WriteLine("\nPlease choose an operation:");
         Console.WriteLine("1. Addition");  
         Console.WriteLine("2. Subtraction");
+        Console.WriteLine("3. Multiplication");
+        Console.WriteLine("4. Division");
         Console.WriteLine("0. Exit");
 
         int choice = Convert.ToInt32(Console.ReadLine());
@@ -14,6 +16,12 @@
                 break;
             case 2:
                 Subtraction();
+                break;
+            case 3:
+                Multiplication();
+                break;
+            case 4:
+                Division();
                 break;
             case 0:
                 Console.WriteLine("Exiting the program. Goodbye!");
@@ -39,5 +47,29 @@
             Console.WriteLine("Enter the second number:");
             double num2 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine($"Result: {num1 - num2}");
+        }
+        static void Multiplication()
+        {
+            Console.WriteLine("Enter the first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Result: {num1 * num2}");
+        }
+
+        static void Division()
+        {
+            Console.WriteLine("Enter the dividend:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the divisor:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            if (num2 == 0)
+            {
+                Console.WriteLine("Error: Division by zero not allowed.");
+            }
+            else
+            {
+                Console.WriteLine($"Result: {num1 / num2}");
+            }
         }
     }
