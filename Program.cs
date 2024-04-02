58c9ae8c49ba2912c7a25f88e43539cc82289fa5
Thosoky <ryabonova@mail.ru>
2024-04-02 23:20:24 +0700

    add Addition

--- Program.cs
+++ Program.cs
@@ -1,18 +1,43 @@
 ﻿ while (true)
     {
         Console.WriteLine("\nPlease choose an operation:");
+        Console.WriteLine("1. Addition");  
+        Console.WriteLine("2. Subtraction");
         Console.WriteLine("0. Exit");
 
         int choice = Convert.ToInt32(Console.ReadLine());
 
         switch (choice)
         {
-            
+            case 1:
+                Addition();
+                break;
+            case 2:
+                Subtraction();
+                break;
             case 0:
                 Console.WriteLine("Exiting the program. Goodbye!");
                 return;
             default:
                 Console.WriteLine("Invalid choice. Please try again.");
                 break;
+        }
+        
+        static void Addition()
+        {
+            Console.WriteLine("Enter the first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Result: {num1 + num2}");
+        }
+
+        static void Subtraction()
+        {
+            Console.WriteLine("Enter the first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Result: {num1 - num2}");
         }
     }
