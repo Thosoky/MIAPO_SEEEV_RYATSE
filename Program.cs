0c6cb9c7fce7dc89843e2ca30f41cb75fe2f5d2c
Thosoky <ryabonova@mail.ru>
2024-04-02 23:13:37 +0700

    я устала

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿ while (true)
+    {
+        Console.WriteLine("\nPlease choose an operation:");
+        Console.WriteLine("0. Exit");
+
+        int choice = Convert.ToInt32(Console.ReadLine());
+
+        switch (choice)
+        {
+            
+            case 0:
+                Console.WriteLine("Exiting the program. Goodbye!");
+                return;
+            default:
+                Console.WriteLine("Invalid choice. Please try again.");
+                break;
+        }
+    }
